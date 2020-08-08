e52627c97b3ebe545484516fefb67d97ba77439d
mk2 <66389650+mk2crypto@users.noreply.github.com>
2020-08-07 23:37:00 -0400

    Update dashboardwidget.cpp

--- src/qt/mk2x/dashboardwidget.cpp
+++ src/qt/mk2x/dashboardwidget.cpp
@@ -442,7 +442,7 @@ void DashboardWidget::changeChartColors()
         backgroundColor = linePenColorY;
         axisY->setGridLineColor(QColor("#1a000000"));
     } else {
-        gridY = QColor("#40ffffff");
+        gridY = QColor("#ffdd65ff");
         axisY->setGridLineColor(gridY);
         gridLineColorX = QColor(15,11,22);
         linePenColorY =  gridLineColorX;
@@ -635,8 +635,8 @@ void DashboardWidget::onChartRefreshed()
     // init sets
     set0 = new QBarSet(CURRENCY_UNIT.c_str());
     set1 = new QBarSet("z" + QString(CURRENCY_UNIT.c_str()));
-    set0->setColor(QColor(92,75,125));
-    set1->setColor(QColor(176,136,255));
+    set0->setColor(QColor(202,158,0));
+    set1->setColor(QColor(255,229,135));
 
     if (!series) {
         series = new QBarSeries();
